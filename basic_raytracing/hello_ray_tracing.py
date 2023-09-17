b26148a6b56fb87bb087b8951ba91624090e177e
Juanjo Tugores <tugorez@gmail.com>
2023-09-17 16:24:36 -0700

    Very very basic (prolly wrong) raytracer

--- basic_raytracing/hello_ray_tracing.py
+++ basic_raytracing/hello_ray_tracing.py
@@ -1,21 +1,54 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
+
+VIEWPORT_DISTANCE = 1
+
+VIEWPORT = (64, 64)
+
+CANVAS = (VIEWPORT[0] + 1, VIEWPORT[1] + 1)
+
+# Just dots atm
+SCENE = [
+    [-i, 0, 1] for i in range(32)
+]
 
 def magnitude(v):
     return math.sqrt(sum([vi * vi for vi in v]))
-
 
 def normalize(v):
     m = magnitude(v)
     return v / m
 
 def same_direction(a, b):
+    a = np.array(a)
+    b = np.array(b)
     na = normalize(a)
     nb = normalize(b)
-    return na == nb
+    e = ((na - nb) ** 2).sum()
+    return e <= 0.0001
+
+def get_color(canvas): 
+    i, j = canvas
+    # change the coordinates system :")
+    cx = j - VIEWPORT[0] / 2
+    cy = VIEWPORT[1] / 2 - i
+    # compute the viewport point
+    vx = cx * VIEWPORT[0] / CANVAS[0]
+    vy = cy * VIEWPORT[1] / CANVAS[1]
+    vz = VIEWPORT_DISTANCE
+    v = (vx, vy, vz)
+
+    for dot in SCENE:
+        if same_direction(dot, v):
+            return (0, 255, 0)
+    return (0, 0, 0)
 
 if __name__ == '__main__':
-    a = np.array([4, 5])
-    b = np.array([8, 10])
-    print(same_direction(a, b))
-
+    canvas = np.zeros(CANVAS + (3,))
+    for i in range(CANVAS[0]):
+        for j in range(CANVAS[1]):
+            color = get_color((i, j))
+            canvas[i][j] = color
+    plt.imshow(canvas, vmin = 0, vmax = 255, interpolation = None)
+    plt.show()
